0500553757cfbee0f1fbd63692c67451e67ec2e4
ibnakhal <inakhal2@705rmBILLY.students.aac.academyart.edu>
2015-12-17 16:49:58 -0800

    Fixes to Level 5 Mesh, creation of level 6

--- Assets/Scripts/World/Teleporter.cs
+++ Assets/Scripts/World/Teleporter.cs
@@ -12,25 +12,32 @@ public class Teleporter : MonoBehaviour {
     private AudioSource teleport;
     [SerializeField]
     private Vector3 rotation;
+    [SerializeField]
+    private Renderer rend;
+    [SerializeField]
+    private float scrollSpeed;
+    [SerializeField]
+    private string target;
 
+    // Use this for initialization
+    void Start () {
 
-	// Use this for initialization
-	void Start () {
-        
-	}
+        rend.GetComponent<Renderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
-	}
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset,0));
+    }
 
 
 
 
     public void OnTriggerEnter(Collider Other)
     {
-        if(open && Other.tag == "Player")
+        if(open && Other.tag == target)
         {
             Other.transform.position = exit.position;
             //Other.GetComponent<FirstPersonController>().enabled = false;
@@ -41,4 +48,8 @@ public class Teleporter : MonoBehaviour {
         }
        // Other.GetComponent<FirstPersonController>().enabled = true;
     }
+
+
+
+
 }
